7f7adf701dc26dedc3b53ff9428db486430415af
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-08 14:51:35 +0100

    Get endpoints

--- kitchensync-api/routes/api.php
+++ kitchensync-api/routes/api.php
@@ -3,6 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Get all accounts
+Route::get('/accounts', function () {
+    $accounts = DB::select('SELECT * FROM accounts');
+    return response()->json($accounts);
+});
+
+// Get accounts by id
+Route::get('/accounts/{id}', function ($id) {
+    $account = DB::select('SELECT * FROM accounts WHERE id = ?', [$id]);
+    if(empty($account)) {
+        return response()->json(['message' => 'Account not found'], 404);
+    } else {
+        return response()->json($account[0]);
+    }
+});
+
