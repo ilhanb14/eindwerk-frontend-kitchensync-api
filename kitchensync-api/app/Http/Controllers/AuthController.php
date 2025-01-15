9969fc8d66846379f88b323070bed8f0b57b05f5
litchitsme <karelsegers@outlook.com>
2025-01-15 14:46:24 +0100

    Split name into first_name and last_name for verifyToken function

--- kitchensync-api/app/Http/Controllers/AuthController.php
+++ kitchensync-api/app/Http/Controllers/AuthController.php
@@ -56,7 +56,8 @@ class AuthController extends Controller
             return response()->json([
                 'valid' => true,
                 'user' => [
-                    'name' => $user->name,
+                    'first_name' => $user->first_name,
+                    'last_name' => $user->last_name,
                     'email' => $user->email
                 ]
             ]);
