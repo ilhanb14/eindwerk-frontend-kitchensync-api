50c21a8c1f6c4de993915ccc6a03a4ecc58845cc
litchitsme <karelsegers@outlook.com>
2025-01-24 10:19:46 +0100

    Added errors in the invite function and added an error code to the errors to handle them better in angular

--- kitchensync-api/app/Http/Controllers/InvitationController.php
+++ kitchensync-api/app/Http/Controllers/InvitationController.php
@@ -7,25 +7,63 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use App\Models\Status;
+use Illuminate\Support\Facades\Validator;
 
 class InvitationController extends Controller
 {
     public function invite(Request $request)
     {   
-        // Validate id from inviter
-        $request->validate([
+        // Validate email
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+        ]);
+
+        // Error if email is not in the right format
+        if ($validator->fails()) {
+            return response()->json([
+                'error_code' => 'INVALID_EMAIL_FORMAT',
+                'message' => 'Invalid email format',
+            ], 400);
+        }
+
+        // Validate IDs
+        $validator = Validator::make($request->all(), [
             'inviter_id' => 'required|exists:users,id',
             'family_id' => 'required|exists:families,id',
         ]);
 
-        // Validate email from incoming request and get the id of the person attached
-        $invitee = User::where('email', $request->validate([
-            'email' => 'required|email|exists:users,email',
-            ]))->first();
-
+        // Error if id's can't be found
+        if ($validator->fails()) {
+            return response()->json([
+                'error_code' => 'INVALID_ID',
+                'message' => 'Invalid id'
+            ], 400);
+        }
+        
         // Check if user exists
+        $invitee = User::where('email', $request->input('email'))->first();
         if (!$invitee) {
-            return response()->json(['message' => 'User not found'], 404);
+            return response()->json([
+                'error_code' => 'USER_NOT_FOUND',
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        // Check if user is already in family
+        if ((string) $invitee->family_id === $request->input('family_id')) {
+            return response()->json([
+                'error_code' => 'USER_ALREADY_IN_FAMILY',
+                'message' => 'User already in family',
+            ], 400);
+        }
+
+        // Check if user is already invited by this family
+        $existingInvitation = Invitation::where('invitee_id', $invitee->id)->where('family_id', $request->input('family_id'))->first();
+        if ($existingInvitation) {
+            return response()->json([
+                'error_code' => 'USER_ALREADY_INVITED',
+                'message' => 'User already invited',
+            ], 400);
         }
 
         // Create a new invitation
@@ -36,7 +74,9 @@ class InvitationController extends Controller
         $invitation->family_id = $request->input('family_id');
         $invitation->save();
 
-        return response()->json(['message' => 'Invitation sent successfully!']);
+        return response()->json([
+            'message' => 'Invitation sent successfully!'
+        ], 200);
     }
 
     public function respond($invitationId, Request $request)
