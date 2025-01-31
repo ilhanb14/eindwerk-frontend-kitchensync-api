4d5b9620cfac31b801f99e51eca894f93220004c
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-31 12:59:31 +0100

    cuisine to cuisine_id in request model

--- kitchensync-api/app/Models/Request.php
+++ kitchensync-api/app/Models/Request.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Request extends Model
 {
-    protected $fillable = ['date', 'meal_id', 'mealtime_id', 'family_id', 'user_id', 'comment', 'cuisine'];
+    protected $fillable = ['date', 'meal_id', 'mealtime_id', 'family_id', 'user_id', 'comment', 'cuisine_id'];
 
     // Disable timestamps
     public $timestamps = false;
